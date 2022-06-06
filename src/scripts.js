c1dbadc5af0a79798a20a12154b5b245fdc5696f
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-06-06 15:54:02 -0600

    Added basic login page info

--- src/scripts.js
+++ src/scripts.js
@@ -9,3 +9,22 @@ import './images/turing-logo.png'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+
+const loginForm = document.getElementById("loginForm");
+const loginButton = document.getElementById("loginFormSubmit");
+const loginErrorMsg = document.getElementById("loginErrorMsg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+    // if password === "travel" && if username === 'traveler' + valid id
+    // => take them to the dashboard of their id. 
+    // if (username === "user" && password === "web_dev") {
+    //     alert("You have successfully logged in.");
+    //     location.reload();
+    // } else {
+    //     loginErrorMsg.style.opacity = 1;
+    // }
+})
