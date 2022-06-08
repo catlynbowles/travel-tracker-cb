daf1217098bcb47249190dac2cdfadbad229ffcd
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-06-08 11:34:49 -0600

    Added getPastTrips() method

--- src/Trip.js
+++ src/Trip.js
@@ -7,8 +7,27 @@ class Trip {
     let userTripData = this.data.filter(trip => {
       return trip.userID === id
     })
-    console.log(userTripData)
+    // console.log(userTripData)
     return userTripData
+  }
+
+  getCurrentDate() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = yyyy + '/' + mm + '/' + dd;
+    // console.log(today)
+    return today
+  }
+
+  getPastTrips(userTripData, currentDate) {
+    let pastTrips = userTripData.filter(trip => {
+      return trip.date < currentDate
+    })
+    // console.log(pastTrips)
+    return pastTrips
   }
 }
 
--- test/Trip-test.js
+++ test/Trip-test.js
@@ -5,7 +5,8 @@ describe('Trip', () => {
   let trip;
   let tripData;
   let singleTraveler;
-  let userTripData1, userTripData2
+  let userTripData1, userTripData2;
+  let date;
 
   beforeEach(() => {
     tripData = [{
@@ -97,7 +98,18 @@ describe('Trip', () => {
       "duration": 17,
       "status": "approved",
       "suggestedActivities": []
-    }]
+    },
+    {
+      "id": 86,
+      "userID": 22,
+      "destinationID": 46,
+      "travelers": 4,
+      "date": "2023/11/27",
+      "duration": 10,
+      "status": "approved",
+      "suggestedActivities": []
+    }
+  ]
     singleTraveler = {
       "id": 22,
       "name": "Gus Courtenay",
@@ -111,6 +123,7 @@ describe('Trip', () => {
     trip = new Trip(tripData);
     userTripData1 = trip.getUserTripData(22);
     userTripData2 = trip.getUserTripData(18);
+    date = trip.getCurrentDate();
   });
 
   it('should be a function', function () {
@@ -122,75 +135,150 @@ describe('Trip', () => {
     expect(trip).to.be.an.instanceof(Trip);
   });
 
-  it('should be able to filter trips based on userID', () => {
+  it('should be able to find all user trips based on userID', () => {
     expect(userTripData1).to.be.an('array');
     expect(userTripData1).to.deep.equal([
-    {
-      id: 22,
-      userID: 22,
-      destinationID: 9,
-      travelers: 4,
-      date: '2022/05/01',
-      duration: 19,
-      status: 'approved',
-      suggestedActivities: []
-    },
-    {
-      id: 113,
-      userID: 22,
-      destinationID: 6,
-      travelers: 3,
-      date: '2019/08/14',
-      duration: 15,
-      status: 'approved',
-      suggestedActivities: []
-    },
-    {
-      id: 86,
-      userID: 22,
-      destinationID: 46,
-      travelers: 4,
-      date: '2020/10/31',
-      duration: 17,
-      status: 'approved',
-      suggestedActivities: []
-    }
-  ])
+      {
+        id: 22,
+        userID: 22,
+        destinationID: 9,
+        travelers: 4,
+        date: '2022/05/01',
+        duration: 19,
+        status: 'approved',
+        suggestedActivities: []
+      },
+      {
+        id: 113,
+        userID: 22,
+        destinationID: 6,
+        travelers: 3,
+        date: '2019/08/14',
+        duration: 15,
+        status: 'approved',
+        suggestedActivities: []
+      },
+      {
+        id: 86,
+        userID: 22,
+        destinationID: 46,
+        travelers: 4,
+        date: '2020/10/31',
+        duration: 17,
+        status: 'approved',
+        suggestedActivities: []
+      },
+      {
+        id: 86,
+        userID: 22,
+        destinationID: 46,
+        travelers: 4,
+        date: '2023/11/27',
+        duration: 10,
+        status: 'approved',
+        suggestedActivities: []
+      }
+    ])
 
   expect(userTripData2).to.be.an('array');
   expect(userTripData2).to.deep.equal([
-    {
-      id: 179,
-      userID: 18,
-      destinationID: 21,
-      travelers: 1,
-      date: '2020/10/30',
-      duration: 14,
-      status: 'approved',
-      suggestedActivities: []
-    },
-    {
-      id: 18,
-      userID: 18,
-      destinationID: 2,
-      travelers: 2,
-      date: '2022/09/25',
-      duration: 17,
-      status: 'approved',
-      suggestedActivities: []
-    },
-    {
-      id: 25,
-      userID: 18,
-      destinationID: 12,
-      travelers: 6,
-      date: '2019/10/26',
-      duration: 9,
-      status: 'approved',
-      suggestedActivities: []
-    }
-  ])
+      {
+        id: 179,
+        userID: 18,
+        destinationID: 21,
+        travelers: 1,
+        date: '2020/10/30',
+        duration: 14,
+        status: 'approved',
+        suggestedActivities: []
+      },
+      {
+        id: 18,
+        userID: 18,
+        destinationID: 2,
+        travelers: 2,
+        date: '2022/09/25',
+        duration: 17,
+        status: 'approved',
+        suggestedActivities: []
+      },
+      {
+        id: 25,
+        userID: 18,
+        destinationID: 12,
+        travelers: 6,
+        date: '2019/10/26',
+        duration: 9,
+        status: 'approved',
+        suggestedActivities: []
+      }
+    ])
   });
 
-  
+  // it('should be able to calculate today/s date', () => {
+  //   // let date = trip.getCurrentDate()
+  //   // expect method to return today's date! how can i do this lol
+  // });
+
+  it('should be able find all past trips for the user', () => {
+    let pastTrips1 = trip.getPastTrips(userTripData1, date);
+    expect(pastTrips1).to.be.an('array');
+    expect(pastTrips1).to.deep.equal([
+      {
+        id: 22,
+        userID: 22,
+        destinationID: 9,
+        travelers: 4,
+        date: '2022/05/01',
+        duration: 19,
+        status: 'approved',
+        suggestedActivities: []
+      },
+      {
+        id: 113,
+        userID: 22,
+        destinationID: 6,
+        travelers: 3,
+        date: '2019/08/14',
+        duration: 15,
+        status: 'approved',
+        suggestedActivities: []
+      },
+      {
+        id: 86,
+        userID: 22,
+        destinationID: 46,
+        travelers: 4,
+        date: '2020/10/31',
+        duration: 17,
+        status: 'approved',
+        suggestedActivities: []
+      }
+    ]);
+
+    let pastTrips2 = trip.getPastTrips(userTripData2, date);
+    expect(pastTrips2).to.be.an('array');
+    expect(pastTrips2).to.deep.equal([
+      {
+        id: 179,
+        userID: 18,
+        destinationID: 21,
+        travelers: 1,
+        date: '2020/10/30',
+        duration: 14,
+        status: 'approved',
+        suggestedActivities: []
+      },
+      {
+        id: 25,
+        userID: 18,
+        destinationID: 12,
+        travelers: 6,
+        date: '2019/10/26',
+        duration: 9,
+        status: 'approved',
+        suggestedActivities: []
+      }
+    ]);
+  });
 });
