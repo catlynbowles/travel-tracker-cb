b9cfe4511e670b77951ccd61d407d2a42467728b
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-06-09 14:54:00 -0600

    Add intial version of returnLocationProperties() prepped for DOM display

--- src/Destination.js
+++ src/Destination.js
@@ -13,17 +13,43 @@ class Destination {
   // }
   // what do I want in there?
   // what kind of data? could it be an object with three key values?
-  // destination, date,and image? 
-  returnLocationName(idsArr) {
-    let locationNames = idsArr.reduce((acc, id) => {
-      this.data.forEach(destination => {
-        if (destination.id === id) {
-          acc.push(destination.destination)
+  // destination, date,and image?
+  // returnLocationName(idsArr) {
+  //   let locationNames = idsArr.reduce((acc, id) => {
+  //     this.data.forEach(destination => {
+  //       if (destination.id === id) {
+  //         acc.push(destination.destination)
+  //       }
+  //     })
+  //     return acc
+  //   }, [])
+  //   return locationNames
+  // }
+
+  // returnLocationName(destArr, tripsArr) {
+  //   let tripsObj = tripsArr.reduce((acc, trip) => {
+  //     let destGuys = destArr.forEach(destination => {
+  //       console.log('dest', destination)
+  //     })
+  //     console.log('trip', trip)
+  //     return acc
+  //   }, [])
+  //   return tripsObj
+  // }
+
+  returnLocationProperties(destArr, tripsArr) {
+    let locationProperties = tripsArr.reduce((acc, trip) => {
+      let destProperties = destArr.forEach(destination => {
+        if (trip.destinationID === destination.id) {
+          acc.push({location: destination.destination, date: trip.date, img: destination.image, alt: destination.alt})
         }
       })
+        // console.log('dest', destination)
+      // console.log('trip', trip)
       return acc
-    }, [])
-    return locationNames
+    }, []);
+    // console.log(locationProperties);
+    return locationProperties;
   }
 }
 
